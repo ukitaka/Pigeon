41fceb5b84e471904fc7309b7ce8f752daca9b58
yuki.takahashi <yuki.takahashi.1126@gmail.com>
2017-01-28 22:49:33 +0900

    Add entrypoint

---
+++ src/main/scala/me/waft/pigeon/Main.scala
@@ -0,0 +1,14 @@
+package me.waft.pigeon
+
+class Main extends xsbti.AppMain {
+  def run(configuration: xsbti.AppConfiguration): xsbti.MainResult = {
+    val scalaVersion = configuration.provider.scalaProvider.version
+
+    println("Hello world!  Running Scala " + scalaVersion)
+    configuration.arguments.foreach(println)
+
+    new Exit(0)
+  }
+  class Exit(val code: Int) extends xsbti.Exit
+}
+
