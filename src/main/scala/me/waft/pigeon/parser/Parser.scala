91b8ad83177cc5aa4916af9c29f28348ce751c44
yuki.takahashi <yuki.takahashi.1126@gmail.com>
2017-01-30 22:21:55 +0900

    Add combinators

--- src/main/scala/me/waft/pigeon/parser/Parser.scala
+++ src/main/scala/me/waft/pigeon/parser/Parser.scala
@@ -8,12 +8,24 @@ trait Parser[+A] { lhs =>
   def map[B](f: A => B): Parser[B] = new Parser[B] {
     def run(location: Location) = lhs.run(location).map(f)
   }
+
+  def listOf[A](n: Int): Parser[List[A]] = ???
+
+  def many: Parser[List[A]] = ???
+  def many1: Parser[List[A]] = ???
+
+  def product[B](p: Parser[B]): Parser[(A, B)] = ???
+
+  def slice(): Parser[String] = ???
 }
 
 object Parser {
-  def parseTranslationUnit(string: String): Parser[Seq[DeclTopLevel]] = ???
+  def parseTranslationUnit(string: String): Parser[List[DeclTopLevel]] = ???
 
   def string(s: String): Parser[String] = ???
 
   def char(c: Char): Parser[Char] = string(c.toString).map(_.charAt(0))
+
+  def success[A](a: A): Parser[A] = string("").map(_ => a)
+
 }
