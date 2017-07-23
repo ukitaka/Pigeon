e7fbd3f709816613e82e372e23861564428e36f6
ukitaka <yuki.takahashi.1126@gmail.com>
2017-07-23 23:20:33 +0900

    Add `Sub`

--- src/main/scala/me/waft/pigeon/generator/llvm/Operator.scala
+++ src/main/scala/me/waft/pigeon/generator/llvm/Operator.scala
@@ -5,4 +5,5 @@ trait BinaryOperation
 object BinaryOperation {
   case class Add(op1: Int, op2: Int) extends BinaryOperation
   case class Fadd(op1: Float, op2: Float) extends BinaryOperation
+  case class Sub(op1: Int, op2: Int) extends BinaryOperation
 }
