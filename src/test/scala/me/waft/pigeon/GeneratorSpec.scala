279e4bb6a4c04b8eb13223b5c8cc6d7d040eb6d6
ukitaka <yuki.takahashi.1126@gmail.com>
2017-07-24 18:19:47 +0900

    Add `GeneratorSpec`

---
+++ src/test/scala/me/waft/pigeon/GeneratorSpec.scala
@@ -0,0 +1,10 @@
+package me.waft.pigeon
+
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+
+class GeneratorSpec extends FlatSpec with Matchers {
+  "generator" should "work well" in {
+
+  }
+}
