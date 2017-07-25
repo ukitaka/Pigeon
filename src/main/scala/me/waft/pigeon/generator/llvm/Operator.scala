9d694bf6899011b05f524a22924ecd594a0fdb7a
ukitaka <yuki.takahashi.1126@gmail.com>
2017-07-25 21:20:44 +0900

    Add Operator

--- src/main/scala/me/waft/pigeon/generator/llvm/Operator.scala
+++ src/main/scala/me/waft/pigeon/generator/llvm/Operator.scala
@@ -4,6 +4,7 @@ trait BinaryOperation
 
 object BinaryOperation {
   case class Add(op1: Int, op2: Int) extends BinaryOperation
-  case class Fadd(op1: Float, op2: Float) extends BinaryOperation
+  case class FAdd(op1: Float, op2: Float) extends BinaryOperation
   case class Sub(op1: Int, op2: Int) extends BinaryOperation
+  case class FSub(op1: Float, op2: Float) extends BinaryOperation
 }
